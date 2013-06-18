02be48c6119fcb1caa5b0ee2011054ea470a1977
Andrew <xl3etax@gmail.com>
2013-06-18 06:52:04 -0700

    First commit

---
+++ src/FlvAudioInputStream.java
@@ -0,0 +1,180 @@
+import java.io.IOException;
+import java.io.InputStream;
+
+public class FlvAudioInputStream extends InputStream {
+	private boolean headRead = false;
+	private boolean endOfStream = false;
+	private InputStream input;
+	private long offset = 0L;
+	private int available = 0;//audio bytes
+
+	public FlvAudioInputStream(InputStream input) {
+		this.input = input;
+	}
+
+	public long getOffset() {
+		return offset;
+	}
+
+	public void close() throws IOException {
+		offset = 0L;
+		available = 0;
+		headRead = false;
+		endOfStream = false;
+		input.close();
+	}
+
+	public long skip(long paramLong) throws IOException {
+		if (endOfStream)
+			return 0L;
+		if (!headRead)
+			inputReadHead();
+		long l1 = 0L;
+		while (l1 < paramLong && !endOfStream) {
+			if (available <= 0) available = readTag();
+			long l2 = paramLong - l1;
+			long l3 = input.skip(l2 > available ? available : l2);
+			l1 += l3;
+			available = (int) (available - l3);
+		}
+		return l1;
+	}
+
+	public int read() throws IOException {
+		byte[] bytes = new byte[1];
+		readAudio(bytes, 0, 1);
+		return bytes[0];
+	}
+
+	public int read(byte[] buffer) throws IOException {
+		return read(buffer, 0, buffer.length);
+	}
+
+	public int read(byte[] buffer, int offset, int len) throws IOException {
+		return readAudio(buffer, offset, len);
+	}
+
+	private void inputReadHead() throws IOException {
+		long l1 = readUInt32();
+		if (l1 != 1179407873L)
+			throw new IOException("Not an FLV file.");
+		long l2 = readUInt8();
+		long l3 = readUInt32();
+		headRead = true;
+		_skip(l3 - offset);
+	}
+
+	private int readAudio(byte[] buffer, int offset, int len) throws IOException {
+		if (endOfStream)
+			return -1;
+		if (!headRead)
+			inputReadHead();
+		int i = 0;
+		while (i < len && !endOfStream) {
+			if (available <= 0) {
+				available = readTag();
+			} else {
+				int l = len - i;
+				int read = _read(buffer, offset + i, l > available ? available : l);
+				if (read < 0)
+					break;
+				i += read;
+				available -= read;
+			}
+		}
+		return i;
+	}
+
+	private int readTag() throws IOException {
+		if (endOfStream)
+			return 0;
+		long l1 = readUInt32();
+		long l2 = readUInt8();
+		int i = (int) readUInt24();
+		long l3 = readUInt24();
+		l3 |= readUInt8() << 24;
+		long l4 = readUInt24();
+		if (i == 0)
+			return 0;
+		long l5 = readUInt8();
+		i--;
+		if (l2 == 8L) return i;
+		if (l2 == 9L) {
+			_skip(i);
+			return 0;
+		}
+
+		_skip(i);
+		return 0;
+	}
+
+	private long _skip(long len) throws IOException {
+		long l1 = 0L;
+		while (l1 < len) {
+			long l2 = input.skip(len - l1);
+			l1 += l2;
+			if (l2 <= 0L) break;
+		}
+		offset += l1;
+		return l1;
+	}
+
+	private int _read() throws IOException {
+		int i = input.read();
+		if (i < 0) {
+			endOfStream = true;
+			return -1;
+		}
+		offset += 1L;
+		return i;
+	}
+
+	private int _read(byte[] buffer) throws IOException {
+		return _read(buffer, 0, buffer.length);
+	}
+
+	private int _read(byte[] buffer, int offset, int len) throws IOException {
+		int i = 0;
+		while (i < len) {
+			int l = len - i;
+			int k = input.read(buffer, offset + i, l);
+			if (k < 0) {
+				endOfStream = true;
+				if (i != 0)
+					break;
+				return -1;
+			}
+			i += k;
+		}
+
+		this.offset += i;
+		return i;
+	}
+
+	private long readUInt8() throws IOException {
+		return _read();
+	}
+
+	private long readUInt24() throws IOException {
+		byte[] bytes = new byte[4];
+		int i = _read(bytes, 1, 3);
+		if (i < 3)
+			return 0L;
+		return toUInt32(bytes);
+	}
+
+	private long readUInt32() throws IOException {
+		byte[] arrayOfByte = new byte[4];
+		int i = _read(arrayOfByte, 0, 4);
+		if (i < 4) return 0L;
+		return toUInt32(arrayOfByte);
+	}
+
+	private long toUInt32(byte[] bytes) {
+		long l = (0xFF & bytes[0]) << 24;
+		l |= (0xFF & bytes[1]) << 16;
+		l |= (0xFF & bytes[2]) << 8;
+		l |= 0xFF & bytes[3];
+		return l;
+	}
+}
---
+++ src/YouTube.java
@@ -0,0 +1,292 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+/**
+ * @author l3eta
+ * @version 1.0
+ */
+
+public class YouTube {
+    /*
+    Does not always download, retrying after a couple mins or 30 seconds sometimes helps
+     */
+
+	/**
+	 * Used to prevent bad urls such as 403, 400 exceptions
+	 */
+	public static final ArrayList<String> BAD_KEYS = new ArrayList<String>();
+	//public static final HashMap<Integer, String> FILE_TYPES = new HashMap<Integer, String>();
+
+	static {
+		BAD_KEYS.add("stereo3d");
+		BAD_KEYS.add("type");
+		BAD_KEYS.add("fallback_host");
+		BAD_KEYS.add("quality");
+
+		//These are here in case we ever add in different stream types.
+        /*FILE_TYPES.put(36, "3GP_240p");
+        FILE_TYPES.put(13, "3GP_144p");
+        FILE_TYPES.put(17, "3GP_144p");
+        FILE_TYPES.put(5, "FLV_240p");
+        FILE_TYPES.put(6, "FLV_270p");
+        FILE_TYPES.put(34, "FLV_360p");
+        FILE_TYPES.put(35, "FLV_480p");
+        FILE_TYPES.put(18, "MP4_(Max 480p)");
+        FILE_TYPES.put(22, "MP4_720p");
+        FILE_TYPES.put(37, "MP4_1080p");
+        FILE_TYPES.put(160, "MP4_144p (Video Only)");
+        FILE_TYPES.put(133, "MP4_240p (Video Only)");
+        FILE_TYPES.put(134, "MP4_360p (Video Only)");
+        FILE_TYPES.put(135, "MP4_480p (Video Only)");
+        FILE_TYPES.put(136, "MP4_720p (Video Only)");
+        FILE_TYPES.put(137, "MP4_1080p (Video Only)");
+        FILE_TYPES.put(139, "M4A_48Kbps (Video Only)");
+        FILE_TYPES.put(140, "M4A_128kbps (Audio Only)");
+        FILE_TYPES.put(141, "M4A_256kbps (Audio Only)");
+        FILE_TYPES.put(38, "MP4");
+        FILE_TYPES.put(83, "MP4_3D 240p");
+        FILE_TYPES.put(82, "MP4_3D 360p");
+        FILE_TYPES.put(85, "MP4_3D 520p");
+        FILE_TYPES.put(84, "MP4_3D 720p");
+        FILE_TYPES.put(43, "WEBM_360p");
+        FILE_TYPES.put(44, "WEBM_480p");
+        FILE_TYPES.put(45, "WEBM_720p");
+        FILE_TYPES.put(46, "WEBM_1080p");
+        FILE_TYPES.put(100, "WEBM_3D 360p");
+        FILE_TYPES.put(101, "WEBM_3D 360p");
+        FILE_TYPES.put(102, "WEBM_3D 720p");*/
+	}
+
+	public static void main(String[] args) {
+		downloadUrl("http://m.youtube.com/watch?v=93LJVlxzE_Q");
+	}
+
+	private static int target_quality = 5;//35 does not work (Yet), haven't tested 34,
+	private static Progress progress = new Progress() { //debugging
+		public void update(int current) {
+			System.out.println(current + "/" + total);
+		}
+	};
+
+	public static void setProgress(Progress progress) {
+		YouTube.progress = progress;
+	}
+
+	public static void downloadUrl(String url) {
+		int i = url.indexOf("v=") + 2;
+		int x = url.indexOf("&", i);
+		if (x < 0)
+			x = url.length();
+		download(url.substring(i, x));
+
+	}
+
+	public static void download(String id) {
+		String[] flvUrls = getFLVFormatUrls(getURLS(id));
+
+		if (flvUrls != null) {
+			boolean downloaded = false;
+			for (String flv : flvUrls) {
+				int tag = getTag(flv);
+				if (tag == target_quality) {
+					downloaded = downloadVideo("", flv);
+				}
+				if (!downloaded) {
+					downloaded = downloadVideo("", flv);
+				}
+				if (downloaded)
+					break;
+			}
+		}
+	}
+
+	private static int getTag(String url) {
+		int i;
+		return Integer.parseInt(url.substring(i = url.indexOf("itag=") + 5, url.indexOf('&', i)));
+	}
+
+	private static String[] getFLVFormatUrls(String[] urls) {
+		ArrayList<String> u = new ArrayList<String>();
+		for (String url : urls) {
+			int tag = getTag(url);
+			if (tag == 35 || tag == 34 || tag == 6 || tag == 5) {
+				u.add(url);
+			}
+		}
+		Collections.sort(u, new Comparator<String>() {//my first attempt at actually using this class lolol
+			public int compare(String url, String url1) {
+				int tag = getTag(url), tag1 = getTag(url1);
+				if (tag < tag1)
+					return 1;
+				if (tag > tag1)
+					return -1;
+				return 0;
+			}
+		});
+		return u.toArray(new String[u.size()]);
+	}
+
+	private static String[] getURLS(String id) {
+		try {
+			HttpURLConnection connection = con("http://www.youtube.com/get_video_info?video_id=" + id + "&asv=3&el=detailpage&hl=en_US");
+			connection.setRequestMethod("GET");
+			InputStream is = connection.getInputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+
+			LinkedList<String> urls = new LinkedList<String>();
+			String title = null;
+			for (String l; (l = reader.readLine()) != null; ) {
+				for (String p : l.split("&")) {
+					String key = p.substring(0, p.indexOf('='));
+					String value = p.substring(p.indexOf('=') + 1);
+					if (key.equals("url_encoded_fmt_stream_map")) {
+						value = decode(value);
+						for (String u : value.split("url=")) {
+							u = getCorrectURL(decode(u));
+							if (!u.startsWith("http") && !isValid(u)) {
+								continue;
+							}
+							urls.add(u);
+						}
+					} else if (key.equals("title")) {
+						title = value.replace("+", "%20");
+					}
+				}
+			}
+			if (title == null) {
+				throw new RuntimeException("Failed to find title can't complete url");
+			}
+			String[] url_map = urls.toArray(new String[urls.size()]);
+			for (int i = 0; i < url_map.length; i++) {
+				url_map[i] += url_map[i].endsWith("&") ? "title=" + title : "&title=" + title;
+			}
+			return url_map;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private static boolean isValid(String url) {
+		return url.contains("signature=") && url.contains("factor=");
+	}
+
+	private static HttpURLConnection con(String url) throws Exception {
+		URL u = new URL(url);
+		HttpURLConnection connection = (HttpURLConnection) u.openConnection();
+		connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.3");
+		return connection;
+	}
+
+	private static String getCorrectURL(String input) {
+		StringBuilder builder = new StringBuilder(input.substring(0, input.indexOf('?') + 1));
+
+		String[] params = input.substring(input.indexOf('?') + 1).split("&");
+		LinkedList<String> keys = new LinkedList<String>();
+		boolean first = true;
+		for (String param : params) {
+			String key = param;
+			try {
+				key = param.substring(0, param.indexOf('='));
+			} catch (Exception ex) {
+				System.out.println(param);
+			}
+			if (keys.contains(key) || BAD_KEYS.contains(key)) {
+				continue;
+			}
+			keys.add(key);
+			if (key.equals("sig")) {
+				builder.append(first ? "" : "&").append("signature=").append(param.substring(4));
+			} else {
+				if (param.contains(",quality=")) {
+					param = remove(param, ",quality=", "_end_");
+				}
+				if (param.contains(",type=")) {
+					param = remove(param, ",type=", "_end_");
+				}
+				if (param.contains(",fallback_host")) {
+					param = remove(param, ",fallback_host", ".com");
+				}
+				builder.append(first ? "" : "&").append(param);
+			}
+			if (first)
+				first = false;
+		}
+		return builder.toString();
+	}
+
+	private static String remove(String text, String start, String end) {
+		int l = text.indexOf(start);
+		return text.replace(text.substring(l, end.equals("_end_") ? text.length() : text.indexOf(end, l)), "");
+	}
+
+	public static boolean downloadVideo(String title, String url) {
+		System.out.println("Trying: " + url);
+		FlvAudioInputStream input = null;
+		FileOutputStream output = null;
+		try {
+			String file = getSaveFilePath(title);
+			if (file != null) {
+				output = new FileOutputStream(file);
+				URL u = new URL(url);
+				HttpURLConnection con = (HttpURLConnection) u.openConnection();
+				con.connect();
+				if (progress != null)
+					progress.setTotal(con.getContentLength());
+				input = new FlvAudioInputStream(con.getInputStream());
+				byte[] buffer = new byte[8192];
+				int len;
+				while ((len = input.read(buffer, 0, 8192)) > 0) {
+					output.write(buffer, 0, len);
+					if (progress != null)
+						progress.update((int) input.getOffset());
+				}
+				return true;
+			}
+		} catch (Exception ex) {
+			handleException(ex);
+		} finally {
+			try {
+				if (input != null)
+					input.close();
+				if (output != null)
+					output.close();
+			} catch (IOException ex) {
+				handleException(ex);
+			}
+		}
+		return false;
+	}
+
+	public static void handleException(Exception ex) {
+		System.out.println(ex);
+	}
+
+	public static String getSaveFilePath(String f) {
+		return "C:/Users/l3eta/Desktop/file.mp3";
+	}
+
+	public static String decode(String s) {
+		try {
+			return URLDecoder.decode(s, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return s;
+	}
+
+	public abstract static class Progress {
+		protected int total;
+
+		public void setTotal(int total) {
+			this.total = total;
+		}
+
+		public int getTotalMB() {
+			return total / 1024 / 1024;
+		}
+
+		public abstract void update(int current);
+	}
+}
